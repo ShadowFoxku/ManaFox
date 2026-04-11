3419c62dc938e9d469e2928bb8f48d8708023b59
ShadowFoxku <rokutheshadowfox@gmail.com>
2026-04-11 12:27:56 +1200

    this ain't middleware

--- ManaFox.Hosting.Middleware/DependencyInjection.cs
+++ ManaFox.Hosting.Middleware/DependencyInjection.cs
@@ -7,7 +7,6 @@ namespace ManaFox.Hosting.Middleware
         public static IApplicationBuilder AddManaFoxMiddleware(this IApplicationBuilder app)
         {
             app.UseMiddleware<ErrorHandling.ErrorHandling>();
-            app.UseMiddleware<Conventions.ApplyFromBodyConvention>();
             return app;
         }
     }
